ebee2da0607520f7e8d244f4be46ade162c469bd
KevinPgn <kevin.pigeon2810@gmail.com>
2024-03-21 13:45:56 +0100

    add id

--- src/components/MainComponsants/Entities.tsx
+++ src/components/MainComponsants/Entities.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { useEntities } from "../../store/Entities"
 import { useEvidences } from "../../store/Evidences";
 
 export const Entities = () => {
   const entities = useEntities((state) => state.entities)
   
+  const [isActive, setIsActive] = useState(false)
+  const [selectedEntity, setSelectedEntity] = useState<number | null>(null)
+
   const evidence = useEvidences((state) => state.evidences)
   const notThisEvidences = useEvidences((state) => state.notThisEvidences)
 
@@ -14,13 +18,16 @@ export const Entities = () => {
     })
   }
 
-  console.log(entities);
+  const handleClickActive = (id: number) => {
+    setIsActive(true)
+    setSelectedEntity(id)
+  }
   
 
   return <>
     <div className="entities">
       {filteredEntities().map((entity) => (
-        <div key={entity.id} className="card">
+        <div key={entity.id} className={isActive && entity.id === selectedEntity ? "card active":"card"} onClick={() => handleClickActive(entity.id)}>
           <h2>{entity.name}</h2>
           <p className="preuves">{entity.evidence.map((item) => {
             return <span key={item}>{item}</span>
